632cd21cca9fe3c9a709ee8751fca5a0763fb4d2
Iornav <38967587+Iornav@users.noreply.github.com>
2022-06-25 21:05:40 +0000

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text  = models.TextField()
-    Author =models.ForeignKey(User, on_delete=models.CASCADE)
+    Author =models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     Created_date = models.DateTimeField(auto_now_add=True)
     Published_date = models.DateTimeField(blank=True, null=True)
     
